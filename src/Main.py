08bef65756ca1d123122f421079ed9e0fdd40daf
bogdan-moldoveanu94 <bogdan.andrey56@gmail.com>
2016-09-02 00:51:30 +0300

    Add sentiment analysis demo

--- src/Main.py
+++ src/Main.py
@@ -4,18 +4,30 @@ from scipy import fftpack, misc
 import matplotlib.pyplot as plt
 import mpmath
 from colorsys import hls_to_rgb
+from pymongo import MongoClient
 import Image
 import numpy as np
+from scipy.ndimage.filters import gaussian_filter
 
 def main():
-    face = misc.face(gray=True)
-    misc.imsave('face.png', face)
-    face = misc.imread('DSC_1025.jpg', mode='RGB')
+
+
+    # client = MongoClient()
+    # db = client.fftdb
+    # collection = db.collection
+    # print(collection)
+    face = misc.face()
+    # misc.imsave('face.png', face)
+    blurred = gaussian_filter(face, sigma=7)
+    #io.imsave('gauss.png', blurred)
+
+    # face = misc.imread('DSC_1025.jpg', mode='RGB')
     # print(face)
-    fourierImage = fftpack.fftn(face)**3
-    shiftedFourierImage = fftpack.fftshift(fourierImage)
-    mag = np.log(np.abs(shiftedFourierImage)+1)
-    io.imshow(mag)
+    #fourierImage = fftpack.fftn(face)**3
+    #shiftedFourierImage = fftpack.fftshift(fourierImage)
+    # mag = np.log(np.abs(shiftedFourierImage)+1)
+    # io.imshow(data.clock())
+    io.imshow(face)
     io.show()
 
 #image = data.camera()  # or any NumPy array!
@@ -49,18 +61,6 @@ def main():
 # b = abs(np.fft.rfft2(array))
 # j = Image.fromarray((b))
 # j.save('img2.png')
-def colorize(z):
-    n,m, r = z.shape
-    c = np.zeros((n,m,r))
-    #c[np.isinf(z)] = (1.0, 1.0, 1.0)
-    #c[np.isnan(z)] = (0.5, 0.5, 0.5)
-
-    idx = True
-    A = (np.angle(z[idx]) + np.pi) / (2*np.pi)
-    A = (A + 0.5) % 1.0
-    B = 1.0 - 1.0/(1.0+abs(z[idx])**0.3)
-    c[idx] = [hls_to_rgb(a.all(), b.all(), 0.8) for a, b in zip(A, B)]
-    return c
 
 if __name__ == "__main__":
     main()
