ea17ebcd5734fdcd925aa06f1f3f4a7a2a1531f7
bogdan-moldoveanu94 <bogdan.andrey56@gmail.com>
2016-09-03 20:00:23 +0300

    Bugfix

--- src/FFTApp/routes.py
+++ src/FFTApp/routes.py
@@ -5,9 +5,9 @@ UPLOAD_FOLDER = '/src/FFTApp/uploads'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
@@ -21,3 +21,17 @@ def uploaded_file(filename):
 @app.route('/uploads/<filename>')
 def send_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
+
+@app.route('/start', methods=['POST'])
+def get_counts():
+    # get url
+    data = json.loads(request.data.decode())
+    url = data["url"]
+    if 'http://' not in url[:7]:
+        url = 'http://' + url
+    # start job
+    job = q.enqueue_call(
+        func=count_and_save_words, args=(url,), result_ttl=5000
+    )
+    # return created job id
+    return job.get_id()
